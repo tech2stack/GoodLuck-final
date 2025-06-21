d8c6ced69dfea33fde98a44050834027f06135dd
Tech2Stack <kunalnagwanshi94@gmail.com>
2025-06-22 01:35:26 +0530

    test4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,6 @@ import OverallReportsComponent from './components/reports/OverallReportsComponen
 import BranchOverviewReport from './components/reports/BranchOverviewReport';
 // import BranchDetailsReport from './components/reports/BranchDetailsReport';
 // import ReportsHub from './pages/ReportsHub';
-//asdf
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
